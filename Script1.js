4255baf25e8a448867b69eec895a3e0de3e69f04
Cameron McGlasson <ClashM@users.noreply.github.com>
2021-05-04 06:58:45 -0700

    Working now

--- Script1.js
+++ Script1.js
@@ -1,4 +1,4 @@
 // JavaScript source code
-console.log("Testing");
-console.error("This is an error!");
-console.warn("This is a public service announcment this is only a test.");
+console.log('Testing');
+console.error('This is an error!');
+console.warn('This is a public service announcment this is only a test.');
